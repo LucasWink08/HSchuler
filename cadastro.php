fe91718a56c4895c84796a26cbb2fb6a0e933c04
Lucas <lucascwinkrsl@gmail.com>
2026-03-25 21:39:40 -0300

    uptade no login

--- cadastro.php
+++ cadastro.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastro</title>
-    <link rel="stylesheet" type="text/css" href="estilo_login.css">
+    <link rel="stylesheet" type="text/css" href="estilo_cadastro.css">
 </head>
 <body>
 <nav>
